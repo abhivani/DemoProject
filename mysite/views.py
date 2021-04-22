60d42cc528974fd8ab759bc0d188a2503bc55251
abhishek vani <abhishekvani80@gmail.com>
2021-04-22 18:50:22 +0530

    djnago project

---
+++ mysite/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render (request,'mysite/index.html')
+
+
+
+def login(request):
+    return render(request,'mysite/login.html')
+
+
+
+
