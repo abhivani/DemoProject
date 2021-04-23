17657393e9bd770492bc663bf3d9de8bd13a976b
abhishek vani <abhishekvani80@gmail.com>
2021-04-23 08:58:00 +0530

    Login and Signup works

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.email
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from  .models import Student
+import requests
+
 
 def index(request):
     return render (request,'mysite/index.html')
@@ -6,8 +9,28 @@ def index(request):
 
 
 def login(request):
-    return render(request,'mysite/login.html')
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print("Login Sucessfull")
+        return redirect('mysite/join.html')
+    else:
+        print('user is not valid.')
+        return render(request,'mysite/login.html')
 
 
 
+
+def signup(request):
+    if request.method == 'POST':
+        fname_r = request.POST.get('fname')
+        lname_r = request.POST.get('lname')
+        email_r = request.POST.get('email')
+        password_r = request.POST.get('password')
+
+        data = Student(email = email_r, fname= fname_r, lname = lname_r, password=password_r)
+        data.save()
+        return render(request, 'mysite/login.html')
+    else:
+        return render(request, 'mysite/signup.html')
 
